b349db1cdc51bae9ae518dc30f5510506a0f46a7
Sohail <888chilly@gmail.com>
2017-01-12 02:33:13 -0500

    more memory code

--- src/mem.cpp
+++ src/mem.cpp
@@ -1,6 +1,33 @@
 #include "mem.h"
 
-void NesMEM::write(uint16_t addr, uint8_t value)
+//Constructor
+//TODO
+NesMEM::NesMEM()
 {
     
+}
+
+//Deconstructor
+//TODO
+NesMEM::~NesMEM()
+{
+    
+}
+/** Translates a NES memory address to a computer memory address
+*/
+void* NesMEM::get_mem_location(uint16_t addr)
+{
+    //TODO unimplemented
+    return 0;
+}
+void NesMEM::write(uint16_t addr, uint8_t value)
+{
+    uint8_t* pointer = get_mem_location(addr);
+    *pointer = value;
+}
+
+uint8_t NesMEM::read(uint16_t addr)
+{
+    uint8_t* pointer = get_mem_location(addr);
+    return (*pointer);
 }
